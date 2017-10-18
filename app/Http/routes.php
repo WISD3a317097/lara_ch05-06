31fac6eed4b41b4c6aa11b52d366b44ca4214ccd
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-18 14:17:10 +0800

    路由命名

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,12 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix'=>'student'],function(){
-    Route::get('{student_no}',function($student_no){
-        return "學號:".$student_no;
-    });
-    Route::get('{student_no}/score/{subject?}',function($student_no,$subject=null){
-        return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
-    })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+    Route::get('{student_no}',[
+        'as'=>'student',
+        'uses'=>function($student_no){
+             return "學號:".$student_no;
+        }
+    ]);
+    Route::get('{student_no}/score/{subject?}',[
+        'as'=>'student.score',
+        'uses'=>function($student_no,$subject=null){
+            return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
+        }
+    ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
 });
 
 
