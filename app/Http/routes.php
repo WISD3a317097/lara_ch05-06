c5cafc1bd324ff0453008668135a610572a1ed66
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-18 14:06:55 +0800

    選擇性路由參數

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,6 @@ Route::get('student/{student_no}',function($student_no){
 Route::get('student/{student_no}/score',function($student_no){
     return "學號:".$student_no."的所有成績";
 });
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+    return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
+});
