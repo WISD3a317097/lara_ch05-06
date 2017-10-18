c584586c70f72d90e2238a0d1bf77bc20189da82
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-18 14:02:45 +0800

    基礎路由參數

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
+});
+Route::get('student/{student_no}/score',function($student_no){
+    return "學號:".$student_no."的所有成績";
+});
